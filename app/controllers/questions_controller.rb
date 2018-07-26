7f82b12c4fe31adad8e112e72643abb7526d8937
Emma Sjoblom <sjoblom.em@gmail.com>
2018-07-26 15:21:18 +0800

    done excersise 1

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:ask].to_s
+    if @question.include? "?"
+      @answer =  "Silly question, get dressed and go to work"
+    elsif @question == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = " I don't care, get dressed and go to work!"
+     end
+   return @answer
+  end
+
+end
